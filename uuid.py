7096a8ccb487c0950f816fea7a90d71482de93ac
san7ket <san7ketjagtap@gmail.com>
2016-08-10 17:42:05 +0530

    Added uuid.py

---
+++ uuid.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import uuid
+import fileinput, sys
+import re
+
+
+filenm = sys.argv[1]
+print sys.argv[1]
+for line in fileinput.input([filenm], inplace=True):
+	uid = str(uuid.uuid4())
+	line = re.sub(r'^@id:\s*(.*)$', '@id: ' + uid, line)
+	#ine = line.replace("@id:", "@id: "+uid)
+	sys.stdout.write(line)
