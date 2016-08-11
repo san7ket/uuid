e7a83fa639153da21cbc7fd2ba90e9a090c4329c
san7ket <san7ketjagtap@gmail.com>
2016-08-11 16:41:27 +0530

    Update uuid.py

--- uuid.py
+++ uuid.py
@@ -1,3 +1,4 @@
+#!/bin/python
 """
 Created By:
 Shubham Vasaikar (vasaikar)
@@ -7,25 +8,41 @@ Sanket Jagtap (sjagtap)
 import uuid
 import os
 import sys
+import argparse
 
-
-newname = sys.argv[1]+".bak"
-os.rename(sys.argv[1], newname)
-print sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--replace", help="replace all @Id")
+parser.add_argument("-f", "--filename", help="filepath")
+args = parser.parse_args()
+newname = args.filename +".bak"
+os.rename(args.filename, newname)
+print args.filename
 
 with open(newname, 'r') as f:
 	content = f.readlines()
-	
-with open(sys.argv[1], 'w') as f1:
-	for line in content:
-		index = line.find('@id:', 0, len(line))
-		if  index != -1:
-			uid = uuid.uuid4()
-			line = line.split(':')[0]
-			line += ": "+str(uid)+"\n"
-			f1.write(line)
-			
-		else:
-			f1.write(line)
 
+if int(args.replace) != 0 :
+	with open(args.filename, 'w') as f1:
+		for line in content:
+			index = line.find('@id:', 0, len(line))
+			if  index != -1:
+				uid = uuid.uuid4()
+				line = line.split(':')[0]
+				line += ": "+str(uid)+"\n"
+				f1.write(line)
+			
+			else:
+				f1.write(line)
+else:
+	with open(args.filename, 'w') as f1:
+		for line in content:
+			index = line.find('@id:', 0, len(line))
+			if  index != -1 and len(line) < 20:
+				uid = uuid.uuid4()
+				line = line.split(':')[0]
+				line += ": "+str(uid)+"\n"
+				f1.write(line)
+			
+			else:
+				f1.write(line)
 os.remove(newname)
