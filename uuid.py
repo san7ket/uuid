c5c2c44254437554f172ebf9dfa940d51d34b164
san7ket <san7ketjagtap@gmail.com>
2016-08-11 12:03:43 +0530

    Update uuid.py

--- uuid.py
+++ uuid.py
@@ -1,19 +1,31 @@
 """
-CreatedBy:
-Sanket Jagtap
-Abhijeet Kasurde
+Created By:
+Shubham Vasaikar (vasaikar)
+Sanket Jagtap (sjagtap)
 """
-#!/bin/python
 
 import uuid
-import fileinput, sys
-import re
+import os
+import sys
 
 
-filenm = sys.argv[1]
+newname = sys.argv[1]+".bak"
+os.rename(sys.argv[1], newname)
 print sys.argv[1]
-for line in fileinput.input([filenm], inplace=True):
-	uid = str(uuid.uuid4())
-	line = re.sub(r'^@id:\s*(.*)$', '@id: ' + uid, line)
-	#ine = line.replace("@id:", "@id: "+uid)
-	sys.stdout.write(line)
+
+with open(newname, 'r') as f:
+	content = f.readlines()
+	
+with open(sys.argv[1], 'w') as f1:
+	for line in content:
+		index = line.find('@id:', 0, len(line))
+		if  index != -1:
+			uid = uuid.uuid4()
+			line = line.split(':')[0]
+			line += ": "+str(uid)+"\n"
+			f1.write(line)
+			
+		else:
+			f1.write(line)
+
+os.remove(newname)
